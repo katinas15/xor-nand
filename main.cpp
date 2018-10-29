8f33b305948d4ea803ace0e175256265ae90fdae
katinas15 <41838906+katinas15@users.noreply.github.com>
2018-10-29 13:51:37 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,29 +3,34 @@
 
 using namespace std;
 
-int XOR(int a, int b){
+bool XOR(int a, int b){
     return a ^ b;
 }
 
-int xor_formule(int a, int b){
-    return ( (a | b) & ~(a & b) );
+bool xor_formule(int a, int b){
+    return (a | b) & ~(a & b);
 }
 
-int nand(int a, int b){
-    int pirmas = XOR(a,b);
-    int antras = XOR(a, pirmas);
-    int trecias = XOR(b, pirmas);
+bool nand(int a,int b){
+    return ~(a & b);
+}
+
+bool nand_schema(int a, int b){
+    bool pirmas = nand(a,b);
+    bool antras = nand(a, pirmas);
+    bool trecias = nand(b, pirmas);
     
-    return XOR(antras, trecias);
+    return nand(antras, trecias);
 }
 
 int main(int argumentCount, char* argumentVector[]) {
-    stringstream ss;
-    ss << argumentVector[1] << ' ' << argumentVector[2];
+//    stringstream ss;
+//    ss << argumentVector[1] << ' ' << argumentVector[2];
+//    int a,b;
+//    ss >> a >> b;
     int a,b;
-    ss >> a >> b;
-    
+    cin>>a>>b;
     cout<<"xor iprastas "<<XOR(a, b)<<endl;
     cout<<"xor formule "<<xor_formule(a, b)<<endl;
-    cout<<"nand "<<nand(a, b)<<endl;
+    cout<<"nand schema"<<nand_schema(a, b)<<endl;
 }
